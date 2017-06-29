6e42fa7d7cc6c055a5183520854f16d4e38c072f
harpole2 <harpole.tyler@gmail.com>
2017-06-29 18:51:24 +0200

    with knn

--- MI_fluc.py
+++ MI_fluc.py
@@ -96,3 +96,34 @@ def flucuationMI_zscore(traj,filename):
 
 
 	np.savetxt(filename+'.txt',zrootsq)
+
+def flucuationMI_knn(traj,filename):
+	nFrames = int(traj.n_frames)
+	sel=traj.topology.select('protein and backbone')
+	traj.superpose(traj,frame=0,atom_indices=sel)
+	nResidues = int(traj.n_residues)
+		
+	points = np.zeros((nFrames,nResidues,3))
+	# Store residue CApoints points
+	for i in range(0,nResidues):
+		tmpTraj_ind = traj.topology.select('protein and resid ' + str(i) + ' and name CA')
+		tmpTraj = traj.atom_slice(tmpTraj_ind)
+		tmpPoints = tmpTraj.xyz
+		points[::,i,::] = np.mean(tmpPoints,axis=1)
+
+	#get the mean position of each point
+	mean_points = np.mean(points,axis=0)
+
+	fluc_points = np.zeros((nFrames,nResidues))
+	for i in range(0, fluc_points.shape[0]):
+		for j in range(0,fluc_points.shape[1]):
+			fluc_points[i,j]=np.absolute(np.linalg.norm(points[i,j]-mean_points[j]))
+
+	#MI based on nearest neighbor
+	MI=np.zeros((nResidues,nResidues))
+	for i in range(0,nResidues):
+		for j in range(i,nResidues):
+			MI[i,j]=mutual_info_regression(np.vstack(fluc_points[:,i]),fluc_points[:,j])
+			MI[j,i]=MI[i,j]
+
+	np.savetxt(filename+'.txt',MI)
